1a3d3257921093619f7bd5fb72c32e8b786284a0
Tomasz Wyrwa <wyrwa.t@gmail.com>
2019-09-06 13:18:18 +0200

    Add state and handleChange to Search,js

--- components/Search.js
+++ components/Search.js
@@ -1,4 +1,17 @@
 Search = React.createClass({
+    getInitialState() {
+        return {
+            searchingText: ''
+        };
+    },
+
+    handleChange: function(event) {
+        var searchingText = event.target.value;
+        this.setState({
+            searchingText: searchingText
+        });
+    },
+
     render: function() {
         var styles = {
             fontSize: '1.5em',
